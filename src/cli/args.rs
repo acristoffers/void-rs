846068195d5aa81d8ebd8b254ebf6fa7d44cb0c2
Álan Crístoffer <acristoffers@gmail.com>
2020-09-20 21:52:16 -0300

    Adds documentation to options.

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -26,34 +26,57 @@ pub use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 pub enum Commands {
+    #[structopt(about = "Creates a new store")]
     Create {
+        #[structopt(help = "Path to the store folder")]
         store_name: String,
     },
+
+    #[structopt(about = "Adds a file or folder to the store")]
     Add {
-        #[structopt(long = "store", short = "s")]
+        #[structopt(help = "store", short = "s", help = "Path to the store folder")]
         store_path: String,
+
+        #[structopt(help = "Path in the store where it will be saved")]
         internal_path: String,
-        #[structopt(required = true)]
+
+        #[structopt(required = true, help = "List of files to insert")]
         files: Vec<String>,
     },
+
+    #[structopt(about = "Get a file or folder from the store (unencrypts it)")]
     Get {
-        #[structopt(long = "store", short = "s")]
+        #[structopt(long = "store", short = "s", help = "Path to the store folder")]
         store_path: String,
+
+        #[structopt(help = "Path in the store where it will be saved")]
         internal_path: String,
+
+        #[structopt(help = "Local path where to decrypt")]
         external_path: String,
     },
+
+    #[structopt(about = "Removes a file or folder from the store")]
     RM {
-        #[structopt(long = "store", short = "s")]
+        #[structopt(help = "store", short = "s", about = "Path to the store folder")]
         store_path: String,
+
+        #[structopt(help = "Path of file or folder to remove from store")]
         path: String,
     },
+
+    #[structopt(about = "List files in the store")]
     LS {
-        #[structopt(short = "v")]
+        #[structopt(short = "v", help = "Prints sizes")]
         verbose: bool,
-        #[structopt(short = "h")]
+
+        #[structopt(short = "h", help = "Prints human-readable sizes")]
         human: bool,
-        #[structopt(long = "store", short = "s")]
+
+        #[structopt(long = "store", short = "s", help = "Path to the store folder")]
         store_path: String,
+
+        #[structopt(help = "Path to list")]
         path: String,
     },
 }
@@ -64,6 +87,6 @@ pub struct Arguments {
     #[structopt(subcommand)]
     pub command: Commands,
 
-    #[structopt(global = true, long = "password", short = "p")]
+    #[structopt(global = true, long = "password", short = "p", help = "Store password")]
     pub password: Option<String>,
 }
