7b65e71c76bf3691ce7e6ba5206cb05a9e2ff0be
Álan Crístoffer <acristoffers@startmail.com>
2022-06-02 17:27:01 +0200

    Updates flutter plataforms.

--- flutter/windows/flutter/generated_plugin_registrant.cc
+++ flutter/windows/flutter/generated_plugin_registrant.cc
@@ -2,14 +2,16 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
 #include <desktop_window/desktop_window_plugin.h>
-#include <file_selector_windows/file_selector_plugin.h>
+#include <file_selector_windows/file_selector_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   DesktopWindowPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("DesktopWindowPlugin"));
-  FileSelectorPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FileSelectorPlugin"));
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
 }
