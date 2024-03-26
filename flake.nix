e981b3afe73c2ee34c39ae854129bed687bd6d9c
Álan Crístoffer <acristoffers@startmail.com>
2024-03-26 09:17:52 -0300

    chore: lint flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    flake-utils.url = github:numtide/flake-utils;
-    naersk.url = github:nix-community/naersk;
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   outputs = { self, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
